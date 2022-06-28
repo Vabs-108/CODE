409da5796df9972cc56f22eea146ba5680e4f40a
Vabs786 <vaibhavkhanna868@gmail.com>
2022-06-28 14:15:50 +0530

    vabss

--- InsertionSort.java
+++ InsertionSort.java
@@ -12,16 +12,17 @@ class InsertionSort{
         for(int i=0;i<=a.length-1;i++){
             System.out.println(a[i]);
         }
-
+//phela step ye hai ki insertion sort mai ham array ko sorted or unsorted mai divide kerte hai
+//loop will start from 1 to a.length-1
 
         for(int i=1;i<a.length-1;i++){
-            temp=a[i];
-            int j=i-1;
-            while(j>=0&&a[j]>temp){
-                a[j+1]=a[j];
-                j--;
+            temp=a[i];  //isme ham array k second index ki value store ker rahe hai
+            int j=i-1;  //yaha hamne j ki value intialize kerdi
+            while(j>=0&&a[j]>temp){  //agar dono conditions satisfy karengi toh it will enter loop
+                a[j+1]=a[j];         // ye sorted array mai se value lega aur j+1 mai daldega
+                j--;                 //loop ulta chalra hai isliye j--
             }
-            a[j+1]=temp;
+            a[j+1]=temp;  // yaha bhar aake j ki value blank value hai uske ander temp store hojayega
         }
         System.out.println("This is your array after sorting");
         for(int i=0;i<=a.length-1;i++){
